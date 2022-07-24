261f70a8c6421248a99950c5a7713f364e8387b3
LGGC <liran12369@gmail.com>
2022-07-24 13:16:06 +0300

    finalize skeleton files

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page",
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
